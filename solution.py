94dd9347696357fea24e5b4c23eaa9228164a2cc
ShikunCui <90287551+ShikunCui@users.noreply.github.com>
2021-10-01 23:29:27 -0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -63,7 +63,7 @@ def smtp_client(port=1025, mailserver='127.0.0.1'):
     # Fill in start
     quitCommand = 'QUIT\r\n'
     clientSocket.send(quitCommand.encode())
-    #recv7 = clientSocket.recv(1024).decode()
+    # recv7 = clientSocket.recv(1024).decode()
     # Fill in end
 
 
